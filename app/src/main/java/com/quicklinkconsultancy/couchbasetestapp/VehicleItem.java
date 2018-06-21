6278f860c1ddf58f825ffbe5c0869ff2de44480e
BRIJESH <brije111@gmail.com>
2018-06-21 13:37:53 +0530

    Initial commit

---
+++ app/src/main/java/com/quicklinkconsultancy/couchbasetestapp/VehicleItem.java
@@ -0,0 +1,27 @@
+package com.quicklinkconsultancy.couchbasetestapp;
+
+public class VehicleItem {
+    String name;
+    String img;
+
+    public VehicleItem(String name, String img) {
+        this.name = name;
+        this.img = img;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+}
